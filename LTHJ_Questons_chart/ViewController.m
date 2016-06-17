c3ce15fd57cf4426d1f48a30b9e8b864af1b938e
蔡春雷 <ccl@caichunleidemacbook-pro.local>
2016-06-17 11:21:33 +0800

    Initial Commit

---
+++ LTHJ_Questons_chart/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LTHJ_Questons_chart
+//
+//  Created by 蔡春雷 on 16/6/17.
+//  Copyright © 2016年 CAICL. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ LTHJ_Questons_chart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LTHJ_Questons_chart
+//
+//  Created by 蔡春雷 on 16/6/17.
+//  Copyright © 2016年 CAICL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
